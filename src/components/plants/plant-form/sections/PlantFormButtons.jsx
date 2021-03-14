4bc04dd9c26b06943ef1ed7dfc920df0a432a813
Sebastian Lunz <lunz.sebastian@gmail.com>
2021-03-14 17:44:48 +0100

    Change position of form buttons.

--- src/components/plants/plant-form/sections/PlantFormButtons.jsx
+++ src/components/plants/plant-form/sections/PlantFormButtons.jsx
@@ -1,28 +1,40 @@
 import React from "react";
-import { Button, FormGroup } from "reactstrap";
+import { Button, FormGroup, Row, Col } from "reactstrap";
 import PropTypes from 'prop-types';
 
 /**
  * @component
  */
-const PlantFormButtons = ({ cancelLabel, submitDisabled, submitLabel, deleteLabel, onDelete }) => {
+const PlantFormButtons = (props) => {
+  const {
+    cancelLabel,
+    submitDisabled,
+    submitLabel,
+    deleteLabel,
+    onDelete,
+    onBackToList } = props;
+
   return (
     <React.Fragment>
       <hr className="mb-4 mt-4" />
-      <FormGroup className="mb-2">
-        <Button color="primary" type="submit" disabled={ submitDisabled }>
-          { submitLabel }
-        </Button>
-        <Button color="secondary" type="reset" className="ml-0 ml-md-2">
-          { cancelLabel }
-        </Button>
-        {
-          onDelete &&
-          <Button color="danger" type="button" className="ml-0 ml-md-2" onClick={onDelete}>
-            { deleteLabel }
+      <Row form className="mb-2">
+        <Col xs={ 4 }>
+          <Button color="secondary" type="button" onClick={ onBackToList }>
+            { cancelLabel }
           </Button>
-        }
-      </FormGroup>
+        </Col>
+        <Col xs={ 8 } className="mb-0 d-flex flex-row-reverse">
+          <Button color="primary" type="submit" className="ml-0 ml-md-2" disabled={ submitDisabled }>
+            { submitLabel }
+          </Button>
+          {
+            onDelete &&
+            <Button color="danger" type="button" className="ml-0 ml-md-2" onClick={ onDelete }>
+              { deleteLabel }
+            </Button>
+          }
+        </Col>
+      </Row>
     </React.Fragment>
   );
 };
